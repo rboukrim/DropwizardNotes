7a7036ed6ada42ec9aa4f93f879e7b13f351deba
Rachid Boukrim <rboukrim@gmail.com>
2016-12-04 21:43:45 -0800

    added mysql database config to application congif yaml, and parsed it in NotesConfiguration

--- notes/src/main/java/com/bmv/notes/NotesConfiguration.java
+++ notes/src/main/java/com/bmv/notes/NotesConfiguration.java
@@ -1,10 +1,26 @@
 package com.bmv.notes;
 
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class NotesConfiguration extends Configuration {
-    // TODO: implement service configuration
+	@NotNull
+	@Valid
+	private DataSourceFactory dataSourceFactory 
+			= new DataSourceFactory();
+	
+	@JsonProperty("database")
+	public DataSourceFactory getDataSourceFactory() {
+		return dataSourceFactory;
+	}
+	
+	@JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory factory) {
+        this.dataSourceFactory = factory;
+    }
 }
